6934fc78c0ddf93b74fa79ed42483d92e52964bd
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2022-06-26 00:07:00 +0300

    Updates

---
+++ precache-manifest.98080f65818baf39498405e4980a75b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e76ed0f3080d2df74bec35624c257f",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "36131dd308c63920efeb",
+    "url": "/todoList/static/css/main.c772371a.chunk.css"
+  },
+  {
+    "revision": "4e0044aabe0d0c4638a3",
+    "url": "/todoList/static/js/2.f19a5572.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todoList/static/js/2.f19a5572.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36131dd308c63920efeb",
+    "url": "/todoList/static/js/main.a26c0d7e.chunk.js"
+  },
+  {
+    "revision": "c39c464575b971d22f9a",
+    "url": "/todoList/static/js/runtime-main.67bcfed6.js"
+  }
+]);
